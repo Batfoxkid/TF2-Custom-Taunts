6ded404e33ec66155a357a70dfef18d048d9b82f
Batfoxkid <batfoxkid@gmail.com>
2020-03-01 19:25:46 -0800

    Add Gamedata

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,7 +1,9 @@
 cd build
 
 mkdir -p package/addons/sourcemod/plugins
+mkdir -p package/addons/sourcemod/gamedata
 mkdir -p package/addons/sourcemod/configs
 
 cp -r addons/sourcemod/plugins/tf2_custom_taunts.smx package/addons/sourcemod/plugins
+cp -r addons/sourcemod/gamedata/tf2.tauntem.txt package/addons/sourcemod/gamedata
 cp -r addons/sourcemod/configs/customtaunts.cfg package/addons/sourcemod/configs
