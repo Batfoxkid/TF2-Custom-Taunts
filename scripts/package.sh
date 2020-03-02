5a48e910379c3d659ee1b466407fc0ab419db8d5
Batfoxkid <batfoxkid@gmail.com>
2020-03-01 19:35:56 -0800

    Actually Actually Fix Packages

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,5 +5,5 @@ mkdir -p package/addons/sourcemod/gamedata
 mkdir -p package/addons/sourcemod/configs
 
 cp -r addons/sourcemod/plugins/tf2_custom_taunts.smx package/addons/sourcemod/plugins
-cp -r ../addons/sourcemod/gamedata addons/sourcemod
-cp -r ../addons/sourcemod/configs addons/sourcemod
+cp -r ../addons/sourcemod/gamedata/tf2.tauntem.txt package/addons/sourcemod/gamedata
+cp -r ../addons/sourcemod/configs/customtaunts.cfg package/addons/sourcemod/configs
