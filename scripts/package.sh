555089be2610740863ee9ee582d41c9aaf217f26
Batfoxkid <batfoxkid@gmail.com>
2020-03-01 18:28:37 -0800

    GitHub Actions

---
+++ scripts/package.sh
@@ -0,0 +1,7 @@
+cd build
+
+mkdir -p package/addons/sourcemod/plugins
+mkdir -p package/addons/sourcemod/configs
+
+cp -r addons/sourcemod/plugins/tf2_custom_taunts.smx package/addons/sourcemod/plugins
+cp -r addons/sourcemod/configs/customtaunts.cfg package/addons/sourcemod/configs
